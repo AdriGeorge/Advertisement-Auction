8d8551a66dd435d2723cbf4c9cbf2b338bd90831
AdriGeorge <adrian7ko@gmail.com>
2020-11-29 09:50:44 +0100

    added function in js to interact with smart contract

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,15 @@ import getWeb3 from './getWeb3';
 import './App.css';
 
 class App extends Component {
-  state = { web3: null, accounts: null, contract: null };
+  state = {
+    web3: null,
+    accounts: null,
+    contract: null,
+    cost: 0,
+    ad: {},
+    contractBalance: 0,
+    adCount: 1,
+  };
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
@@ -34,8 +42,73 @@ class App extends Component {
     }
   };
 
+  // get methods
+
+  getCost = async () => {
+    const { accounts, contract } = this.state;
+    const result = await contract.methods.getCost().call({ from: accounts[0] });
+    this.setState({ cost: result });
+    console.log('🚀 ~ file: App.js ~ line 51 ~ App ~ getCost= ~ cost', cost);
+  };
+
+  getAd = async () => {
+    const { accounts, contract } = this.state;
+    const result = await contract.methods.getAd().call({ from: accounts[0] });
+    this.setState({ ad: result });
+    console.log('🚀 ~ file: App.js ~ line 58 ~ App ~ getAd= ~ ad', ad);
+  };
+
+  getAdCount = async () => {
+    const { accounts, contract } = this.state;
+    const result = await contract.methods
+      .getAdCount()
+      .call({ from: accounts[0] });
+    this.setState({ adCount: result });
+    console.log(
+      '🚀 ~ file: App.js ~ line 65 ~ App ~ getAdCount= ~ adCount',
+      adCount
+    );
+  };
+
+  getBalance = async () => {
+    const { accounts, contract } = this.state;
+    const result = await contract.methods
+      .getBalanceOfContract()
+      .call({ from: accounts[0] });
+    this.setState({ contractBalance: result });
+    console.log(
+      '🚀 ~ file: App.js ~ line 72 ~ App ~ getBalance= ~ contractBalance',
+      contractBalance
+    );
+  };
+
+  // interact methods
+
+  changeAd = async (link, nameLink, value) => {
+    const { accounts, contract } = this.state;
+    await contract.methods
+      .changeAd(link, nameLink)
+      .send({ from: accounts[0], value: value });
+  };
+
+  withdraw = async (amount) => {
+    if (amount >= this.state.contractBalance) return;
+    await contract.methods.withdraw(web3.toWei(amount, 'ether'));
+    this.getBalance();
+  };
+
   render() {
     if (!this.state.web3) {
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
+      console.log('🚀 ~ file: App.js ~ line 72 ~ App ~ getAd= ~ ad', ad);
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return <h2>Every thing is ok, check your console.</h2>;
